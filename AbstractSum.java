f4f6667375444bee0a0ea745d2bcf2dc41321c4a
RishabhKumr <kumarrishabh201@gmail.com>
2020-01-24 17:40:07 +0530

    added main function

--- AbstractSum.java
+++ AbstractSum.java
@@ -3,4 +3,21 @@ abstract class Addit
 {
 abstract int getVal();
 }
-
+class ab extends Addit
+{
+int sum;
+int getVal(int a, int b)
+{
+sum = a + b;
+return sum;
+}
+public static void main(String args[])
+{
+ab obj = new ab();
+Scanner scn = new Scanner(System.in);
+int a = scn.nextInt();
+int b = scn.nextInt();
+int sum = obj.getVal(a,b);
+System.out.println(sum);
+}
+}
